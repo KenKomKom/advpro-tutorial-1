915e021edb234c134fd906159ac3f35068a773dc
KenKomKom <kenichikomala@gmail.com>
2024-03-01 15:49:34 +0700

    [REFACTOR] Added PaymentMethod and PaymentStatus

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 
 import java.util.Map;
@@ -11,6 +13,7 @@ public class Payment {
     String method;
     Map<String, String> paymentData;
     Order order;
+    String status;
 
     public Payment(String id,  Order order, String method, Map<String, String>paymentData){
         this(order, method, paymentData);
@@ -21,11 +24,21 @@ public class Payment {
         this.id = UUID.randomUUID().toString();
         this.method = method;
         this.order = order;
-        setPaymentData(paymentData);
+        try{
+            setPaymentData(paymentData);
+            setStatus(PaymentStatus.SUCCESS.getValue());
+        }catch (IllegalArgumentException e){
+            setStatus(PaymentStatus.REJECTED.getValue());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void setStatus(String status){
+        this.status=status;
     }
 
     private void setPaymentData(Map<String, String>paymentData){
-        if (method.equals("VOUCHER")){
+        if (method.equals(PaymentMethod.VOUCHER.getValue())){
             int numOfNumerics = 0;
             for (int i=0; i<paymentData.get("voucherCode").length(); i++){
                 if (Character.isDigit(paymentData.get("voucherCode").charAt(i))){
@@ -37,7 +50,7 @@ public class Payment {
                     numOfNumerics!=8){
                 throw new IllegalArgumentException();
             }
-        }else if (method.equals("BANK")){
+        }else if (method.equals(PaymentMethod.BANK.getValue())){
             if (paymentData.get("bankName").isBlank() ||
                     paymentData.get("referenceCode").isBlank()){
                 throw new IllegalArgumentException();
