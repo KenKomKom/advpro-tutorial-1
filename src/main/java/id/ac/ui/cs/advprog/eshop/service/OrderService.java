cc4c401cb70dcaecd1758988785e1f6473184339
KenKomKom <kenichikomala@gmail.com>
2024-03-02 15:45:15 +0700

    [GREEN] Added service & repo decorator

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
@@ -1,9 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface OrderService {
     Order createOrder(Order order);
     Order updateStatus(String orderId, String status);
